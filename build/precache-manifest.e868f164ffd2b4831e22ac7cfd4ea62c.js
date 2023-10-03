4cef452b244662b64b035bd00d18e1de319a8b7c
Sai Sathish <sai.sathish@modivcare.com>
2023-10-04 00:55:08 +0530

    first commit

---
+++ build/precache-manifest.e868f164ffd2b4831e22ac7cfd4ea62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448211c33c45c22ac32cd1f77fbebbd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2434eff8e5d51b70195",
+    "url": "/static/css/main.48a36670.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2434eff8e5d51b70195",
+    "url": "/static/js/main.912b5722.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  }
+]);
